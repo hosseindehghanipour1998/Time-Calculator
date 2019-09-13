7dd4b758dd5e32dc455318798843620b5766508a
Hossein <hossein.dehghanipour1998@gmail.com>
2019-09-14 00:52:50 +0430

    Added timeSubtractor , printer , timeExtractor  + tested All of them

--- Python/TimeCalculator.py
+++ Python/TimeCalculator.py
@@ -1,24 +1,92 @@
+def timeExtractor(time):
+    timeList = time.split(":")
+    timeListInt = [ int(timeList[0]) , int(timeList[1]) ]
+    return timeListInt
+#==============================================================================
+def printer (*args):
+    for num in args:
+        print(str(num) + "    ", end = '')
+    print()
+#==============================================================================
 def timeAdder( time1 , time2 ):
-    firstTime = time1.split(":")
-    firstHours = int(firstTime[0])
-    firstMinutes = int(firstTime[1])
+    firstTime = timeExtractor(time1)
+    firstHours = firstTime[0]
+    firstMinutes = firstTime[1]
 
-    secondTime = time2.split(":")
-    secondHours = int(secondTime[0])
-    secondMinutes = int(secondTime[1])
-
+    secondTime = timeExtractor(time2)
+    secondHours = secondTime[0]
+    secondMinutes = secondTime[1]
+    #Operation
     totalHours = firstHours + secondHours
     totlaMinutes = firstMinutes + secondMinutes
     totalHours += totlaMinutes/60 ;
     totlaMinutes = totlaMinutes % 60 ;
     return ("%02d : %02d" %(totalHours,totlaMinutes) )
 
+#==============================================================================
 
-
+def timeSubtractor( time1 , time2 ):
+    firstTime = timeExtractor(time1)
+    firstHours = firstTime[0]
+    firstMinutes = firstTime[1]
+    
+    secondTime = timeExtractor(time2)
+    secondHours = secondTime[0]
+    secondMinutes = secondTime[1]
+    
+    
+    #$printer ( firstTime , firstHours ,firstMinutes,secondTime,secondHours ,secondMinutes)
+    
+    #Get The Max and Min
+    minHours = maxHours = minMinutes = maxMinutes = 0 
+    
+    if ( firstHours > secondHours ):
+        #first time is bigger than the second time
+        maxHours = firstHours
+        maxMinutes = firstMinutes
+        minHours = secondHours
+        minMinutes = secondMinutes
+        
+    elif ( secondHours > firstHours ):
+        #second time is bigger than the first time
+        maxHours = secondHours
+        maxMinutes = secondMinutes
+        minHours = firstHours
+        minMinutes = firstMinutes
+        
+    #if hours are equal then we should compare them by minutes
+    elif ( secondHours ==  firstHours ):
+        if ( firstMinutes > secondMinutes ):
+            #first time is bigger than the second time
+            maxHours = firstHours
+            maxMinutes = firstMinutes
+            minHours = secondHours
+            minMinutes = secondMinutes
+        else:
+            #second time is bigger than the first time
+            maxHours = secondHours
+            maxMinutes = secondMinutes
+            minHours = firstHours
+            minMinutes = firstMinutes
+                
+            
+    #calculate 
+    finalHours = maxHours - minHours 
+    finalMinutes = maxMinutes - minMinutes
+    printer(maxHours,maxMinutes,minHours,minMinutes)
+    if ( finalMinutes < 0 ):
+        finalMinutes += 60 ;
+        finalHours -= 1 ;
+    return ("%02d : %02d" %(finalHours,finalMinutes) )
+        
+ #=============================================================================   
+    
+    
 #Main
 def main():
     print("Hello User")
-    print(timeAdder("1:45" ,"0:0"))
+    #print(timeAdder("1:45" ,"0:0"))
+    #print(timeSubtractor("1:45" ,"12:0"))
    
 
 main()
